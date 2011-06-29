2a4fc8b74e419f2c458944f20c97f9b8f19a67ef
Zdeněk Žabokrtský <zabokrtsky@ufal.mff.cuni.cz>
2011-06-29 21:26:50 +0000

    collecting data size statistics of normalized treebanks

---
+++ statistics/datasize.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Treex;
+use Treex::Core::Document;
+
+my $main_data_dir = "$ENV{TMT_ROOT}/share/data/resources/normalized_treebanks";
+
+foreach my $lang_data_dir (sort glob "$main_data_dir/*") {
+    my ($lang_code) = reverse split /\//,$lang_data_dir;
+    print $lang_code;
+    foreach my $purpose (qw(train test)) {
+        my ($sentences,$tokens) = (0,0);
+        foreach my $file (glob "$lang_data_dir/treex/000_orig/$purpose/*.treex") {
+            my $doc = Treex::Core::Document->new( { filename => $file } );
+            foreach my $bundle ($doc->get_bundles) {
+                $sentences++;
+                $tokens += $bundle->get_zone($lang_code, 'orig')->get_atree->get_descendants;
+#            print "$file loaded\n";
+            }
+        }
+        print "\t$sentences\t$tokens";
+    }
+    print "\n";
+}
