2fbcadeb9d7cc17c94ed23b9872a07257fd4253b
David Mareček <marecek@ufal.mff.cuni.cz>
2011-08-11 12:12:05 +0000

    Malt parser added

--- parsing/mcd_parse.pl
+++ parsing/mcd_parse.pl
@@ -15,19 +15,20 @@ if (!@ARGV) {
 foreach my $language (@ARGV) {
     foreach my $dir (glob "$data_dir/$language/treex/*") {
         next if (!-d $dir);
-        next if (!-e "$dir/mcd.model");
+        next if (!-e "$dir/parsed/mcd_nonproj_o2.model");
         my $name = $dir;
         $name =~ s/^.+\///;
         $name = "$language-$name";
         print STDERR "Creating script for McD parsing $name.\n";
         open (BASHSCRIPT, ">:utf8", "$name-parse.sh") or die;
         print BASHSCRIPT "#!/bin/bash\n\n";
+        print BASHSCRIPT "cp $dir/test/*.treex $dir/parsed/\n";
         print BASHSCRIPT "treex -s Util::SetGlobal language=$language selector=mcdnonprojo2 \\
   Util::Eval zone='\$zone->remove_tree(\"a\") if \$zone->has_tree(\"a\");' \\
   A2A::CopyAtree flatten=1 \\
-  W2A::ParseMST model=$dir/mcd.model \\
+  W2A::ParseMST model=$dir/mcd_nonproj_o2.model \\
   Eval::AtreeUAS selector=''\\
-  -- $dir/test/*.treex >> uas.txt";
+  -- $dir/parsed/*.treex >> $dir/parsed/uas.txt";
         close BASHSCRIPT;
         system "qsub -cwd $name-parse.sh";
     }
---
+++ parsing/train.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+use Treex::Core::Config;
+
+my $data_dir = Treex::Core::Config::share_dir()."/data/resources/normalized_treebanks/";
+my $mcd_dir  = $ENV{TMT_ROOT}."/libs/other/Parser/MST/mstparser-0.4.3b";
+my $malt_dir = $ENV{TMT_ROOT}."/share/installed_tools/malt_parser/malt-1.5";
+
+my ($help, $mcd, $malt);
+
+GetOptions(
+    "help|h" => \$help,
+    "mcd"    => \$mcd,
+    "malt"   => \$malt,
+);
+
+
+if ($help || !@ARGV) {
+    die "Usage: train.pl [OPTIONS] [LANGUAGES]
+    LANGUAGES  - list of ISO codes of languages to be processed
+    --mst      - train McDonald's MST parser
+    --malt     - train Malt parser
+    -h,--help  - print this help
+";
+}
+
+
+foreach my $language (@ARGV) {
+    foreach my $dir (glob "$data_dir/$language/treex/*") {
+        next if (!-d $dir);
+        my $name = $dir;
+        $name =~ s/^.+\///;
+        $name = "$language-$name";
+        system "mkdir -p $dir/parsed";
+        if (!-e "$dir/parsed/train.conll") {
+            print STDERR "CoNLL file not found. Will be created.\n";
+            system "treex Write::CoNLLX language=$language -- $dir/train/*.treex > $dir/parsed/train.conll";
+        }
+        if ($mcd) {
+            print STDERR "Creating script for training McDonald's parser ($name).\n";
+            open (BASHSCRIPT, ">:utf8", "mcd-$name.sh") or die;
+            print BASHSCRIPT "#!/bin/bash\n\n";
+            print BASHSCRIPT "python $mcd_dir/bin/conll2mst.py $dir/parsed/train.conll > $dir/parsed/train.mst\n";
+            print BASHSCRIPT "java -cp $mcd_dir/output/mstparser.jar:$mcd_dir/lib/trove.jar -Xmx2g mstparser.DependencyParser \\\n";
+            print BASHSCRIPT "  train order:2 format:MST train-file:$dir/parsed/train.mst model-name:$dir/mcd_nonproj_o2.model\n";
+            close BASHSCRIPT;
+            system "qsub -cwd mcd-$name.sh";
+        }
+        if ($malt) {
+            print STDERR "Creating script for training Malt parser ($name).\n";
+            open (BASHSCRIPT, ">:utf8", "malt-$name.sh") or die;
+            print BASHSCRIPT "#!/bin/bash\n\n";
+            print BASHSCRIPT "java -Xmx10g -jar $malt_dir/malt.jar -i $dir/parsed/train.conll -c $dir/parsed/malt_stackeager -a stackeager -l liblinear -m learn\n";
+            close BASHSCRIPT;
+            system "qsub -cwd malt-$name.sh";
+        }
+    }
+}
