73b5dc3e71595db4707520204cac306321d4162b
David Mareček <marecek@ufal.mff.cuni.cz>
2011-09-26 12:44:25 +0000

    script for running transformations of coordinations updated doesn't work yet

--- transformations/transform.pl
+++ transformations/transform.pl
@@ -5,14 +5,17 @@ use warnings;
 use Getopt::Long;
 use Treex::Core::Config;
 
-my ($parallel,$help,$all,$alll,$allt);
+my ($parallel, $help, $alll, $family, $punctuation, $conjunction, $head, $shared);
 
 GetOptions(
     "help|h" => \$help,
     "parallel|p" => \$parallel,
-    "all|a" => \$all,
     "alll" => \$alll,
-    "allt" => \$allt,
+    "family=s" => \$family,
+    "punctuation=s" => \$punctuation,
+    "conjunction=s" => \$conjunction,
+    "head=s" => \$head,
+    "shared=s" => \$shared,
 );
 
 sub error {
@@ -26,9 +29,7 @@ sub usage {
     die "Usage:
   transform.pl [OPTIONS] [LANGUAGES] [TRANSFORMERS]
      LANGUAGES     - list of ISO codes of languages to be processed
-     TRANSFORMERS  - list of transformation blocks (short module names, without namespace)
      --alll        - apply the listed transformations on all languages
-     --allt        - apply all transformations on the listed languages
      -a,--all      - apply all transformations on all languages (implies --alll and --allt)
      -p,--parallel - submit transformations to the cluster
      -h,--help  -  print this help.
@@ -64,23 +65,17 @@ if ($help) {
     usage('');
 }
 
-my @transformers;
 my @languages;
 
 my @available_languages = find_available_languages();
-my @available_transformers = find_available_transformers();
 
 my @listed_languages;
-my @listed_transformers;
 foreach my $arg (@ARGV) {
     if ($arg =~ /^[a-z]{2,3}$/) {
         push @listed_languages, $arg;
     }
-    elsif ($arg=~ /[A-Z]\w+/) {
-        push @listed_transformers, $arg;
-    }
     else {
-        error("Unrecognized argument '$arg'. Not an option, not a language code, not a transformer name.\n");
+        error("Unrecognized argument '$arg'. Not an option, not a language code.\n");
     }
 
 };
@@ -91,9 +86,9 @@ foreach my $language (@listed_languages) {
     }
 }
 
-if ($alll or $all) {
+if ($alll) {
     if ( @listed_languages) {
-        error "No language list can be specified if --all or --alll is present";
+        error "No language list can be specified if --alll is present";
     }
     @languages = @available_languages;
 }
@@ -101,45 +96,29 @@ else {
     @languages = @listed_languages;
 }
 
-if ($allt or $all) {
-    if (@listed_transformers) {
-        error "No transformation list can be specified if --all or --allt is present";
-    }
-    @transformers = @available_transformers;
-}
-else {
-    @transformers = @listed_transformers;
-}
 
 if (not @languages) {
     error "Languages to be processed must be specified, either by listing them or by --alll or --all options.";
 }
 
-if (not @transformers) {
-    error "Some transformations must be specified, either by listing them or by --allt or --all options.";
-}
-
-
-my $tasks = @transformers * @languages;
-
-print STDERR "Total number of transformation tasks: ".
-    scalar(@languages)." language(s) * ".scalar(@transformers)." transformation(s) = $tasks task(s)\n";
-
-my $current_task;
-
-foreach my $transformer (@transformers) {
-    foreach my $language (@languages) {
-        $current_task++;
-#        my $command_line = "treex A2A::Transform::$transformer -- $data_dir/$language/treex/001_pdtstyle/*/*treex";
-
-        my $command_line = 'treex '.($parallel?'-p --jobs 5 ':'')
-            . " Util::Eval bundle='\$bundle->remove_zone(qw($language),qw(orig))' " # remove the original trees (before PDT styling)
-            . "A2A::CopyAtree source_language=$language language=$language selector=before " # storing trees before transformation
-                ."A2A::Transform::$transformer language=$language "
-                    . "Util::Eval document='my \$path=\$document->path; \$path=~s/00._pdtstyle/trans_$transformer/;use File::Path qw(mkpath); mkpath(\$path);\$document->set_path(\$path);' "
-                        . " Write::Treex -- $data_dir/$language/treex/*_pdtstyle/*/*.treex.gz &";
-        print STDERR "Executing task $current_task/$tasks\n $command_line\n\n";
-        system $command_line;
-        sleep 2; # wait few secs, so the jobs can be send to the cluster
+foreach my $f (split /,/, $family) {
+    foreach my $p (split /,/, $punctuation) {
+        foreach my $c (split /,/, $conjunction) {
+            foreach my $h (split /,/, $head) {
+                foreach my $s (split /,/, $shared) {
+                    my $name = 'f'.uc(substr($f,0,1)).'p'.uc(substr($p,0,1).'c'.uc(substr($c,0,1).'h'.uc(substr($h,0,1).'s'.uc(substr($s,0,1);
+                    my $command_line = 'treex '.($parallel?'-p --jobs 5 ':'')
+                                     . " Util::Eval bundle='\$bundle->remove_zone(qw($language),qw(orig))' " # remove the original trees (before PDT styling)
+                                     . "A2A::CopyAtree source_language=$language language=$language selector=before " # storing trees before transformation
+                                     . "A2A::Transform::CoordStyle family=$f punctuation=$p conjunction=$c head=$h shared=$s language=$language "
+                                     . "Util::Eval document='my \$path=\$document->path; \$path=~s/00._pdtstyle/trans_$name/;use File::Path qw(mkpath); mkpath(\$path);\$document->set_path(\$path);' "
+                                     . " Write::Treex -- $data_dir/$language/treex/*_pdtstyle/*/*.treex.gz &";
+                    print STDERR "Executing task $language,$name\n $command_line\n\n";
+                    system $command_line;
+                    sleep 2; # wait few secs, so the jobs can be send to the cluster
+                }
+            }
+        }
     }
 }
+
