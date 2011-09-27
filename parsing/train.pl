9e2be1b00dd6dfffc420b7e1fd4228d01860d343
David Mareček <marecek@ufal.mff.cuni.cz>
2011-09-27 21:50:45 +0000

    bugfix

--- parsing/train.pl
+++ parsing/train.pl
@@ -72,7 +72,7 @@ foreach my $language (@ARGV) {
             my $command =  "treex -p -j 20 ";
                $command .= "Util::SetGlobal language=$language ";
                $command .= "Util::Eval anode='\$anode->set_afun(\"Atr\");' ";
-               $command .= "Write::CoNLLX $f deprel_attribute=$deprel_attribute is_member_within_afun=1 is_shared_modifier_within_afun=1 is_coord_conjunction_within_afun=1";
+               $command .= "Write::CoNLLX $f deprel_attribute=$deprel_attribute is_member_within_afun=1 is_shared_modifier_within_afun=1 is_coord_conjunction_within_afun=1 ";
                $command .= "-- $dir/train/*.treex.gz > $dir/parsed/$trainfilename";
             system $command;   
         }
