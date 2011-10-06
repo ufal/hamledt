e02011fb077a645a9b06e910f9483ba495a534b8
Daniel Zeman <zeman@ufal.mff.cuni.cz>
2011-10-06 20:04:59 +0000

    Podstatne rozsireno status.pl

--- parsing/status.pl
+++ parsing/status.pl
@@ -5,10 +5,14 @@
 
 sub usage
 {
-    my $usage = "status.pl --wdir experimentRootFolder\n";
+    my $usage = "status.pl --wdir experimentRootFolder [OPTIONS] |& tee status.log\n";
     $usage   .= "                 expected storage hierarchy: \${experimentRootFolder}/\${LANGUAGE}/\${TRANSFORMATION}\n";
     $usage   .= "          --eqw-resubmit\n";
     $usage   .= "                 cluster jobs that failed to start, their state is Eqw and their script is known will be resubmitted\n";
+    $usage   .= "          --mem-resubmit\n";
+    $usage   .= "                 cluster jobs that failed because Java did not have enough memory will be resubmitted\n";
+    $usage   .= "          --net-resubmit\n";
+    $usage   .= "                 cluster jobs that failed because of IO/network problems, including sudden deaths, will be resubmitted\n";
     return $usage;
 }
 
@@ -26,13 +30,18 @@ GetOptions
 (
     'help|h'       => \$help,
     'wdir=s'       => \$wdirroot,
-    'eqw-resubmit' => \$eqw_resubmit
+    'eqw-resubmit' => \$eqw_resubmit,
+    'mem-resubmit' => \$mem_resubmit,
+    'net-resubmit' => \$net_resubmit
 );
 
 if($help || !$wdirroot)
 {
     die(usage());
 }
+# Get the list of jobs currently running or waiting on the cluster.
+# We will compare it with the logs we find in the experiment folders.
+my %qstat = cluster::qstat0();
 # Switch to the experiment root folder.
 # Absolutize it first so that we can return there from everywhere.
 $wdirroot = dzsys::absolutize_path($wdirroot);
@@ -62,12 +71,36 @@ foreach my $lang (@languages)
         #system('ls -al');
         # Check that all parser models have been trained.
         my @models = qw(malt_nivreeager.mco malt_stacklazy.mco mcd_proj_o2.model mcd_nonproj_o2.model);
+        my $scripttrans = $trans;
+        $scripttrans =~ s/^trans_//;
+        my %scripts =
+        (
+            'malt_nivreeager.mco'  => "mlt-$lang-$scripttrans.sh",
+            'malt_stacklazy.mco'   => "smf-$lang-$scripttrans.sh",
+            'mcd_proj_o2.model'    => "mcp-$lang-$scripttrans.sh",
+            'mcd_nonproj_o2.model' => "mcd-$lang-$scripttrans.sh"
+        );
         foreach my $model (@models)
         {
+            # Remember for every model of every experiment whether we found it.
+            my $mpath = "$tpath/$model";
+            $models{$mpath}{filename} = $model;
             if(!-e $model)
             {
+                $models{$mpath}{found} = 0;
                 print("$model not found\n");
-                $missing{$model}++;
+                # We are going to search job logs and assign jobs to intended target models.
+                # However, it is possible that a target has no job log because the job did not even start.
+                # In such a case we want to know whether there is the script for the job or not.
+                if(-f $scripts{$model})
+                {
+                    $models{$mpath}{script} = $scripts{$model};
+                    $models{$mpath}{spath} = "$tpath/$scripts{$model}";
+                }
+            }
+            else
+            {
+                $models{$mpath}{found} = 1;
             }
         }
         # Collect numbers of cluster jobs related to this transformation.
@@ -84,19 +117,55 @@ foreach my $lang (@languages)
             {
                 die("Unknown job ID for log $log.");
             }
+            # Classify the job as training or parsing.
+            my ($type, $output);
+            if($script =~ m/^(mlt|smf|mcd|mcp)-/)
+            {
+                my $parser = $1;
+                $type = 'training';
+                # Remember the expected output name for every job.
+                if($parser eq 'mlt')
+                {
+                    $output = 'malt_nivreeager.mco';
+                }
+                elsif($parser eq 'smf')
+                {
+                    $output = 'malt_stacklazy.mco';
+                }
+                elsif($parser eq 'mcd')
+                {
+                    $output = 'mcd_nonproj_o2.model';
+                }
+                else
+                {
+                    $output = 'mcd_proj_o2.model';
+                }
+                # At the same time, for every expected output remember the jobs that attempted to create it
+                # (there may be more than one job that attempted to create a particular model).
+                my $mpath = "$tpath/$output";
+                push(@{$models{$mpath}{jobs}}, $jobid);
+            }
+            else
+            {
+                $type = 'unknown';
+            }
             my %job =
             (
+                'id'     => $jobid,
                 'path'   => $tpath,
+                'log'    => $log,
                 'lpath'  => "$tpath/$log",
                 'spath'  => "$tpath/$script",
                 'script' => $script,
+                'type'   => $type,
+                'output' => $output,
+                'opath'  => "$tpath/$output",
             );
             $jobs{$jobid} = \%job;
         }
     }
 }
 # Are any related jobs still running on the cluster?
-my %qstat = cluster::qstat0();
 my @my_jobs = grep {$qstat{$_}{user} eq $ENV{USER}} (sort {$a<=>$b} (keys(%qstat)));
 print("Current user = $ENV{USER}\n");
 my $n_jobs = scalar(@my_jobs);
@@ -108,29 +177,19 @@ foreach my $jobid (@my_jobs)
     if($job->{state} eq 'r' && exists($jobs{$jobid}))
     {
         print("Job $jobid still running: $jobs{$jobid}{spath}\n");
+        $jobs{$jobid}{running} = 1;
     }
     elsif($job->{state} eq 'Eqw')
     {
         # Even jobs that failed to start may have produced a log file that helps us to bind them to their experiment.
         if(exists($jobs{$jobid}))
         {
+            $jobs{$jobid}{waiting} = 1;
             print("Job $jobid failed to start: $jobs{$jobid}{spath}\n");
             system("qstat -j $jobid | grep 'error reason'");
             if($eqw_resubmit)
             {
-                my $job = $jobs{$jobid};
-                if(-f $job->{spath})
-                {
-                    print("$job->{spath} will be resubmitted.\n");
-                    # Go to the folder of the script so that the logs are written there again.
-                    print("Going back to $job->{path}.\n");
-                    chdir($job->{path}) or die("Cannot change to $job->{path}: $!\n");
-                    # We do not know the original memory requirements for the job.
-                    # Let's reserve a whole 32GB machine for it.
-                    $job->{new_jobid} = cluster::qsub('script' => $job->{script}, 'memory' => '31g');
-                    print("New job id = $job->{new_jobid}\n");
-                    dzsys::saferun("qdel $jobid");
-                }
+                resubmit($jobs{$jobid});
             }
         }
         else
@@ -139,6 +198,107 @@ foreach my $jobid (@my_jobs)
         }
     }
 }
+# Identify jobs that have finished but their output was not found.
+foreach my $jobid (keys(%jobs))
+{
+    my $job = $jobs{$jobid};
+    if(!$job->{running} && !-f $job->{opath})
+    {
+        # Get the full log of the job. Perhaps it contains the reason for the failure.
+        $job->{logtext} = `cat $job->{lpath}`;
+        # Many jobs fail because Java VM does not have enough memory.
+        if($job->{logtext} =~ m/Could not reserve enough space for object heap/)
+        {
+            #print("Missing $job->{opath}, found corresponding log $job->{lpath}: $reason\n");
+            $job->{lowmem} = 1;
+        }
+        # A job may also fail to read the input or write the output (due to network problem).
+        # This is what the Malt parser would say in such a case:
+        elsif($job->{logtext} =~ m/The learner cannot write to the instance file/ ||
+              $job->{logtext} =~ m/error reading zip file/)
+        {
+            $job->{io} = 1;
+        }
+        # NullPointerException in MST Parser reader probably indicates failed reading input.
+        elsif($job->{logtext} =~ m/java\.lang\.NullPointerException/)
+        {
+            $job->{nullpointer} = 1;
+        }
+        # Sometimes the Malt Parser throws this exception. I do not have a solution yet.
+        elsif($job->{logtext} =~ m/The learner class 'org\.maltparser\.ml\.lib\.LibSvm' cannot be initialized./)
+        {
+            $job->{libsvm} = 1;
+        }
+    }
+}
+# Loop over all expected outputs, determine whether they exist and if not, determine the cause.
+my @models = sort(keys(%models));
+foreach my $mpath (@models)
+{
+    my $type = $models{$mpath}{filename};
+    $m_total{$type}++;
+    if($models{$mpath}{found})
+    {
+        $m_found{$type}++;
+    }
+    else
+    {
+        # Identify the most recent job that attempts or attempted to build this model.
+        my @jobids = sort {$b<=>$a} (@{$models{$mpath}{jobs}});
+        if(@jobids)
+        {
+            my $jobid = $jobids[0];
+            my $job = $jobs{$jobid};
+            if($job->{running})
+            {
+                $m_running{$type}++;
+            }
+            else
+            {
+                $m_lost{$type}++;
+                if($job->{lowmem})
+                {
+                    $m_lowmem{$type}++;
+                    resubmit($job) if($mem_resubmit);
+                }
+                elsif($job->{io})
+                {
+                    $m_io{$type}++;
+                    resubmit($job) if($net_resubmit);
+                }
+                elsif($job->{nullpointer})
+                {
+                    $m_nullpointer{$type}++;
+                    resubmit($job) if($net_resubmit);
+                }
+                elsif($job->{libsvm})
+                {
+                    $m_libsvm{$type}++;
+                }
+                else
+                {
+                    $m_sudden{$type}++;
+                    print("-------------\n");
+                    print("SUDDEN DEATH?\n");
+                    print("$job->{mpath}\n");
+                    print("$job->{logtext}");
+                    print("$job->{opath}\n");
+                    resubmit($job) if($net_resubmit);
+                }
+            }
+        }
+        # No job found for the target!
+        else
+        {
+            $m_nojob{$type}++;
+            if($models{$mpath}{spath})
+            {
+                print("No job/log found for script $models{$mpath}{spath}.\n");
+            }
+        }
+    }
+}
+print("====================\n");
 foreach my $state (keys(%jobstates))
 {
     my $estate = $state;
@@ -146,10 +306,47 @@ foreach my $state (keys(%jobstates))
     $estate = 'Eqw (failed to start)' if($estate eq 'Eqw');
     print("Number of jobs in state $estate = $jobstates{$state}\n");
 }
-print("==========\n");
-foreach my $model (sort(keys(%missing)))
+print("====================\n");
+foreach my $mt (sort(keys(%m_total)))
 {
-    print("Missing $model $missing{$model} times.\n");
+    printf("%20s: expected %3d times, found %3d times, nojob %3d times, running %3d times, lost %3d times: lowmem %3d, io %3d, null %3d, svm %3d, suddendeath %3d\n", $mt,
+        $m_total{$mt}, $m_found{$mt}, $m_nojob{$mt}, $m_running{$mt}, $m_lost{$mt},
+        $m_lowmem{$mt}, $m_io{$mt}, $m_nullpointer{$mt}, $m_libsvm{$mt}, $m_sudden{$mt});
+}
+
+
+
+###############################################################################
+# SUBROUTINES
+###############################################################################
+
+
+
+#------------------------------------------------------------------------------
+# Resubmits a job that previously failed.
+#------------------------------------------------------------------------------
+sub resubmit
+{
+    my $job = shift; # ref to hash
+    if(-f $job->{spath})
+    {
+        print("$job->{spath} will be resubmitted.\n");
+        # Go to the folder of the script so that the logs are written there again.
+        print("Going back to $job->{path}.\n");
+        chdir($job->{path}) or die("Cannot change to $job->{path}: $!\n");
+        # We do not know the original memory requirements for the job.
+        # Let's reserve a whole 32GB machine for it.
+        $job->{new_jobid} = cluster::qsub('script' => $job->{script}, 'memory' => '31g');
+        print("New job id = $job->{new_jobid}\n");
+        if($job->{waiting})
+        {
+            dzsys::saferun("qdel $job->{id}") or die;
+        }
+    }
+    else
+    {
+        die("Cannot resubmit job $job->{id} because $job->{spath} does not exist.\n");
+    }
 }
 
 
