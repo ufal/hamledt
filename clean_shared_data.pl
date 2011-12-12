bbbe1d05fe6d46918dda232bfb16849bacf3dc7a
Daniel Zeman <zeman@ufal.mff.cuni.cz>
2011-12-12 17:18:00 +0000

    Cleaning updated: now it also removes *-cluster-run-* in language folders in $TMT_ROOT/... working copy. WARNING! This procedure does not check whether any running job still uses one of the folders.

--- clean_shared_data.pl
+++ clean_shared_data.pl
@@ -12,6 +12,7 @@ use lib '/home/zeman/lib';
 use find;
 
 find::go('/ha/projects/tectomt_shared/data/resources/normalized_treebanks', \&clean);
+find::go('.', \&clean_cluster_run);
 
 sub clean
 {
@@ -25,6 +26,21 @@ sub clean
         print("$cesta/$objekt\n");
         unlink("$cesta/$objekt") or
             print STDERR ("WARNING: Cannot remove file $objekt: $!\n");
+    }
+    return $druh eq 'drx';
+}
+
+sub clean_cluster_run
+{
+    my $cesta = shift;
+    my $objekt = shift;
+    my $druh = shift;
+    # Odstranit *-cluster-run-*. Pozor! Nehlídáme, zda už paralelní úloha, která tuto složku využívala, doběhla!
+    if($druh eq 'drx' && $objekt =~ m/-cluster-run-/)
+    {
+        print("$cesta/$objekt\n");
+        system("rm -rf $cesta/$objekt");
+        return 0;
     }
     return $druh eq 'drx';
 }
