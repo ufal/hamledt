f7b2bdc2caa1f96bdd7f789bf1ca16d2330297b1
Martin Popel <popel@ufal.mff.cuni.cz>
2012-09-19 19:18:02 +0000

    * ru and de not published until the licenses are OK * we can publish conll.gz as well

--- publish_treebanks.sh
+++ publish_treebanks.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
 # From treebanks.ods I have chosen those licences which do not match /LDC|do not distribute/
-PUBLISH="ar cs da nl fi de grc la fa pt ro ru sv ta"
-# el    - can we distribute it?
-# et is - normalization not ready
+PUBLISH="ar cs da nl fi grc la fa pt ro sv ta"
+# de el ru - can we distribute it?
+# et is    - normalization not ready
 #PREFIX=${TMT_ROOT}/share/data/archive/hamledt2011
 IN=${TMT_ROOT}/share/data/resources/hamledt
 OUT=${TMT_ROOT}/share/hamledt
 
 for L in $PUBLISH; do
   mkdir -p $OUT/$L
-  echo cp -r $IN/$L/treex/001_pdtstyle/{train,test} $OUT/$L
-  cp -r $IN/$L/treex/001_pdtstyle/{train,test} $OUT/$L
+  echo $OUT/$L
+  cp -r $IN/$L/{conll,treex/001_pdtstyle}/{train,test} $OUT/$L
+  #cp -r $IN/$L/conll/{train,test} $OUT/$L
 done
