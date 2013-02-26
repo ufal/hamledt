e2d4215a3e96f1a8e3e289f35fba4dbfa716103c
David Mareček <marecek@ufal.mff.cuni.cz>
2013-02-26 14:02:42 +0000

    simplifying the code

--- parsing/parse.pl
+++ parsing/parse.pl
@@ -6,19 +6,20 @@ use warnings;
 use Getopt::Long;
 use Treex::Core::Config;
 
-use lib '/home/zeman/lib';
-use dzsys;
+my $share_dir = Treex::Core::Config->share_dir();
+my $data_dir = "$share_dir/data/resources/hamledt";
+my $script_dir = "/home/marecek/treex/devel/hamledt/parsing";
 
-my $data_dir = Treex::Core::Config->share_dir()."/data/resources/hamledt";
+my ($help, $mcd, $mcdproj, $malt, $maltsmf, $feat, $transformations, $new, $wdirroot);
 
-my ($help, $mcd, $mcdproj, $malt, $maltsmf, $new, $feat, $trans, $wdirroot);
-$feat='conll'; # default
-$trans = '';
+# defaults
+$feat = '_';
+$transformations = '000_orig,001_pdtstyle,trans_fMhLsNcBpP,trans_fMhMsNcBpP,trans_fMhRsNcBpP,trans_fPhLsHcHpB,trans_fPhMsHcHpB,trans_fPhRsHcHpB,trans_fShLsNcBpP,trans_fShMsNcBpP,trans_fShRsNcBpP';
+$wdirroot = '/net/cluster/TMP/marecek/hamledt_parsing';
 $mcd=0;
 $malt=0;
 $mcdproj=0;
 $maltsmf=0;
-
 
 GetOptions(
     "help|h"  => \$help,
@@ -27,7 +28,7 @@ GetOptions(
     "malt"    => \$malt,
     "maltsmf" => \$maltsmf,
     "new"     => \$new,
-    "trans=s"   => \$trans,
+    "trans=s"   => \$transformations,
     "feat=s"  => \$feat,
     "wdir=s"  => \$wdirroot,
 );
@@ -43,59 +44,42 @@ if ($help || !@ARGV) {
     --trans    - select transformations separated by comma. All transformations are run otherwise.
     --feat     - select features conll|iset|_ (conll is default)
     --wdir     - path to working folder
-                 default: $data_dir\${LANGUAGE}/treex/\${TRANSFORMATION}/parsed
-                 if --wdir \$WDIR set: \${WDIR}/\${LANGUAGE}/\${TRANSFORMATION}
-                 i.e. separately from the unparsed data and possibly from other experiments
     -h,--help  - print this help
 ";
 }
 
-my %transformation;
-map {$transformation{$_} = 1} split(/,/, $trans);
-# We will be repeatedly changing dir to $wdirroot/something so we need $wdiroot to be absolute path
-# so we are able to resurface from the previous working folder.
-if ($wdirroot) {
-    $wdirroot = dzsys::absolutize_path($wdirroot);
-}
-my $scriptdir = dzsys::get_script_path();
-# Lazy to enumerate all languages?
-if (scalar(@ARGV)==1 && $ARGV[0] eq 'all') {
-    @ARGV = qw(ar bg bn ca cs da de el en es eu fi grc hi hu it ja la nl pt ro ru sl sv ta te tr);
-}
-
-print STDERR ("Going to parse languages: ", join(', ', @ARGV), "\n");
 foreach my $language (@ARGV) {
-    my $glob = "$data_dir/$language/treex/*";
-    my @glob = glob $glob;
-    die("No directories found: glob = $glob") unless(@glob);
-    foreach my $dir (@glob) {
-        # Get the name of the current transformation.
-        my $name = $dir;
-        $name =~ s/^.+\///;
-        # Skip transformations that we do not want to process now.
-        next if $trans && !$transformation{$name};
-        # Set the path to the folder where the training data and the trained model shall be.
-        my $wdir = $wdirroot ? "$wdirroot/$language/$name" : "$dir/parsed";
-        if(!-d $wdir) {
-            print STDERR ("Directory $wdir not found.\n");
-            next;
-        }
+
+    foreach my $trans ( split(/,/, $transformations)) {
+
+        # From where the data will be taken
+        my $ddir = "$data_dir/$language/treex/$trans";
+        next if (!-d $ddir);
+
+        # Where the data will be written
+        my $wdir = "$wdirroot/$language/$trans";
+        next if (!-d $wdir);
+
+        # Compress transformation names because qstat only shows 10 characters.
+        my $shortname = $trans;
+        $shortname =~ s/^trans_/t/;
+        $shortname =~ s/^00[01]_//;
+        my $tr_style = $trans =~ /^trans_/ ? $trans : '';
+        $tr_style =~ s/^trans_//;
+
         # Chdir to the working folder so that all scripts and logs are also created there.
         chdir($wdir) or die("Cannot change to $wdir: $!");
+
         # Copy test data to the working folder unless it is already there.
         if (!-e "001.treex.gz" || $new) {
-            system "cp $dir/test/*.treex.gz .";
+            system "cp $ddir/test/*.treex.gz .";
             print STDERR "New file created!\n";
         }
 
-        my $tr_style = $name;
-        $tr_style =~ s/^trans_//;
-        $name = "$language-$name";
-
         # the following variable indicates whether we are parsing a transformation or pdtstyle/orig file
-        my $is_transformation = $name =~ /trans_/ ? 1 : 0;
+        my $is_transformation = $trans =~ /trans_/ ? 1 : 0;
 
-        my %parser_selector = (mcd => 'mcdnonprojo2', mcdproj => 'mcdprojo2', malt => 'maltnivreeager', maltsmf => 'maltstacklazy');
+        my %parser_selector = (mcd => 'mcd', mcdproj => 'mcdproj', malt => 'malt', maltsmf => 'maltsmf');
         my %parser_model = (
             mcd     => "$wdir/mcd_nonproj_o2.model",
             mcdproj => "$wdir/mcd_proj_o2.model",
@@ -109,13 +93,13 @@ foreach my $language (@ARGV) {
             maltsmf => "W2A::ParseMalt model=$wdir/malt_stacklazy.mco pos_attribute=conll/pos cpos_attribute=conll/cpos feat_attribute=$feat",
         );
         my %base_parser_block = (
-            mcd     => "W2A::ParseMST model_dir=$wdir/../../001_pdtstyle/parsed model=mcd_nonproj_o2.model decodetype=non-proj pos_attribute=conll/pos",
-            mcdproj => "W2A::ParseMST model_dir=$wdir/../../001_pdtstyle/parsed model=mcd_proj_o2.model decodetype=proj pos_attribute=conll/pos",
-            malt    => "W2A::ParseMalt model=$wdir/../../001_pdtstyle/parsed/malt_nivreeager.mco pos_attribute=conll/pos cpos_attribute=conll/cpos",
-            maltsmf => "W2A::ParseMalt model=$wdir/../../001_pdtstyle/parsed/malt_stacklazy.mco pos_attribute=conll/pos cpos_attribute=conll/cpos feat_attribute=$feat",
+            mcd     => "W2A::ParseMST model_dir=$wdir/../001_pdtstyle model=mcd_nonproj_o2.model decodetype=non-proj pos_attribute=conll/pos",
+            mcdproj => "W2A::ParseMST model_dir=$wdir/../001_pdtstyle model=mcd_proj_o2.model decodetype=proj pos_attribute=conll/pos",
+            malt    => "W2A::ParseMalt model=$wdir/../001_pdtstyle/malt_nivreeager.mco pos_attribute=conll/pos cpos_attribute=conll/cpos",
+            maltsmf => "W2A::ParseMalt model=$wdir/../001_pdtstyle/malt_stacklazy.mco pos_attribute=conll/pos cpos_attribute=conll/cpos feat_attribute=$feat",
         );
-
         my %run_parser = ( mcd => $mcd, mcdproj => $mcdproj, malt => $malt, maltsmf => $maltsmf);
+
         my @scenarios;
         foreach my $parser (qw(mcd mcdproj malt maltsmf)) {
             if (!$run_parser{$parser}) {
@@ -144,17 +128,18 @@ foreach my $language (@ARGV) {
             $base_scenario .= "$base_parser_block{$parser} ";
             push @scenarios, $base_scenario;
         }
-        # Note: the trees in 000_orig should be compared against the original gold tree.
-        # However, that tree has the '' selector in 000_orig (while it has the 'orig' selector elsewhere),
-        # so we do not select 'orig' here.
+
+        # Evaluation
         my $eval_scenario = "Util::SetGlobal language=$language ";
         $eval_scenario .= "Eval::AtreeUAS eval_is_member=1 eval_is_shared_modifier=1 selector='' ";
-        # We evaluate the transformation against 'before' selector, if they are convertyed back to PDT style
+        
+        # We evaluate the transformation against 'before' selector as well (for trees converted back to PDT style)
         if ($is_transformation) {
             $eval_scenario .= "Eval::AtreeUAS eval_is_member=1 eval_is_shared_modifier=1 selector='before' ";
         }
+
         print STDERR "Creating script for parsing.\n";
-        open (BASHSCRIPT, ">:utf8", "parse-$name.sh") or die;
+        open (BASHSCRIPT, ">:utf8", "p-$language-$shortname.sh") or die;
         print BASHSCRIPT "#!/bin/bash\n\n";
         # Debugging message: anyone here eating my memory?
         print BASHSCRIPT "hostname -f\n";
@@ -164,8 +149,7 @@ foreach my $language (@ARGV) {
         }
         print BASHSCRIPT "treex $eval_scenario -- *.treex.gz | tee uas.txt\n";
         close BASHSCRIPT;
-        my $memory = '12g';
-        my $qsub = "qsub -hard -l mf=$memory -l act_mem_free=$memory -cwd -j yes parse-$name.sh";
-        system $qsub;
+        my $memory = '15g';
+        system "qsub -hard -l mf=$memory -l act_mem_free=$memory -cwd -j yes p-$language-$shortname.sh";
     }
 }
--- parsing/print_table.pl
+++ parsing/print_table.pl
@@ -5,17 +5,17 @@ use Getopt::Long;
 use Treex::Core::Config;
 use Text::Table;
 
-use lib '/home/zeman/lib';
-use dzsys;
-
-my $data_dir = Treex::Core::Config->share_dir()."/data/resources/hamledt/";
+my $share_dir = Treex::Core::Config->share_dir();
+my $data_dir = "$share_dir/data/resources/hamledt";
+my $script_dir = "/home/marecek/treex/devel/hamledt/parsing";
 
 my $help;
-my $wdirroot;
+my $wdirroot = '/net/cluster/TMP/marecek/hamledt_parsing';
 my $eval = 'pms';
 my $parser = 'malt';
 my $filename = 'uas.txt';
 my $transform = 'there-and-back';
+my $transformations = '000_orig,001_pdtstyle,trans_fMhLsNcBpP,trans_fMhMsNcBpP,trans_fMhRsNcBpP,trans_fPhLsHcHpB,trans_fPhMsHcHpB,trans_fPhRsHcHpB,trans_fShLsNcBpP,trans_fShMsNcBpP,trans_fShRsNcBpP';
 
 GetOptions(
     "help|h"  => \$help,
@@ -25,7 +25,9 @@ GetOptions(
     "wdir=s"  => \$wdirroot,
     "filename=s" => \$filename,
 );
-my $signif_diff = 0.1; # TODO: Update this value (for each lang) as soon as Loganathan finishes the significance testing.
+
+my $signif_diff = 0.1;
+# TODO: Update this value (for each lang) as soon as Loganathan finishes the significance testing.
 
 if ($help || !@ARGV) {
     die "Usage: print_table.pl [OPTIONS] [LANGUAGES]
@@ -33,19 +35,12 @@ if ($help || !@ARGV) {
     --parser   - parser: 'mcd', 'mcdproj', 'malt', or 'maltsmf'. Default is 'malt'.
     --eval     - type o evaluation: p (parent), pm (parent, is_member), pms (parent, is_member, is_shared_modifier). Default is 'pms'.
     --wdir     - path to working folder
-                 default: $data_dir\${LANGUAGE}/treex/\${TRANSFORMATION}/parsed
-                 if --wdir \$WDIR set: \${WDIR}/\${LANGUAGE}/\${TRANSFORMATION}
-                 i.e. separately from the unparsed data and possibly from other experiments
     -h,--help  - print this help
 ";
 }
-# Lazy to enumerate all languages?
-if (scalar(@ARGV)==1 && $ARGV[0] eq 'all') {
-    @ARGV = qw(ar bg bn ca cs da de el en es eu fi grc hi hu it ja la nl pt ro ru sl sv ta te tr);
-}
 
 my %parser_name = (mcd => 'MST', mcdproj => 'MST-PROJECTIVE', malt => 'MALT-ARC-EAGER', maltsmf => 'MALT-STACK-LAZY');
-my %parser_selector = (mcd => 'mcdnonprojo2', mcdproj => 'mcdprojo2', malt => 'maltnivreeager', maltsmf => 'maltstacklazy');
+my %parser_selector = (mcd => 'mcd', mcdproj => 'mcdproj', malt => 'malt', maltsmf => 'maltsmf');
 
 print "********** $parser_name{$parser} **********\n";
 
@@ -53,23 +48,20 @@ my $table = Text::Table->new('trans', @ARGV, 'better', 'worse', 'average');
 my %value;
 
 foreach my $language (@ARGV) {
-    # Avoid warnings about undefined values in debugging messages.
-    foreach my $dir (glob "$data_dir/$language/treex/*") {
-        next if (!-d $dir);
-        # Get the name of the current transformation.
-        my $trans = $dir;
-        $trans =~ s/^.+\///;
+
+    foreach my $trans ( split(/,/, $transformations)) {
+
+        # From where the data will be taken
+        my $wdir = "$wdirroot/$language/$trans";
+        next if (!-d $wdir);
+
         my $is_trans = $trans =~ /^trans_/ ? 1 : 0;
-        # Set the path to the folder where the training data and the trained model shall be.
-        my $wdir = $wdirroot ? "$wdirroot/$language/$trans" : "$dir/parsed";
-        if(!-d $wdir) {
-            print("Directory $wdir not found.\n");
-            next;
-        }
-        if(!open(UAS, "<:utf8", "$wdir/$filename"))
-        {
-            ###!!! DEBUG
-            print("Cannot read $wdir/$filename: $!");
+
+        # Chdir to the working folder so that all scripts and logs are also created there.
+        chdir($wdir) or die("Cannot change to $wdir: $!");
+
+        if (!open(UAS, "<:utf8", "$wdir/uas.txt")) {
+            print STDERR "Cannot read $wdir/uas.txt: $!";
             next;
         }
         my $my_score;
@@ -80,13 +72,10 @@ foreach my $language (@ARGV) {
             if ($is_trans && $transform ne 'there-only' && $sys eq "UAS$eval(".$language."_".$parser_selector{$parser}."PDT,".$language."_before)") {
                 $value{$trans}{$language} += $score ? 100 * $score : 0;
             }
-            elsif ($is_trans && $transform ne 'there-only' && $sys eq "UAS$eval(".$language."_".$parser_selector{$parser}."BASE,".$language."_before)") {
-                $value{$trans}{$language} -= $score ? 100 * $score : 0;
-            }
             elsif ($is_trans && $transform eq 'there-only' && $sys eq "UAS$eval($language"."_$parser_selector{$parser},$language)") {
                 $value{$trans}{$language} += $score ? 100 * $score : 0;
             }
-            elsif ($is_trans && $transform eq 'there-only' && $sys eq "UAS$eval(".$language."_".$parser_selector{$parser}."BASE,".$language."_before)") {
+            elsif ($is_trans && $sys eq "UAS$eval(".$language."_".$parser_selector{$parser}."BASE,".$language."_before)") {
                 $value{$trans}{$language} -= $score ? 100 * $score : 0;
             }
             elsif ($trans eq "001_pdtstyle" && $sys eq "UAS$eval(".$language."_".$parser_selector{$parser}.",".$language.")") {
@@ -96,11 +85,6 @@ foreach my $language (@ARGV) {
                 $value{$trans}{$language} = $score ? 100 * $score : 0;
             }
         }
-        if(!defined($value{$trans}{$language}))
-        {
-            ###!!! DEBUG
-            #print("$parser_name{$parser} score not found in $wdir/$filename.\n");
-        }
     }
 }
 
@@ -109,7 +93,6 @@ sub round {
     my $score = shift;
     return undef if not defined $score;
     return sprintf("%.2f", $score);
-
 }
 
 foreach my $trans (sort keys %value) {
@@ -133,9 +116,10 @@ foreach my $trans (sort keys %value) {
 }
 
 my $langs = @ARGV;
-if ($langs < 18){
+if ($langs < 18) {
     say $table;
-} else {
-    say $table->select(0 .. ($langs/2)+1);
-    say $table->select(0,($langs/2)+2 .. $langs+3);
+}
+else {
+    say $table->select(0 .. ($langs / 2 + 1));
+    say $table->select(0, ($langs / 2 + 2) .. ($langs + 3));
 }
