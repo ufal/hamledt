1637433b3b369bdcff64fd6dd9a3221b485b7959
Daniel Zeman <zeman@ufal.mff.cuni.cz>
2014-04-04 12:22:16 +0000

    HamleDT testing adapted to multiple treebanks per language.

--- tests/summarize_tests.pl
+++ tests/summarize_tests.pl
@@ -9,29 +9,29 @@ while (<>) {
     next unless(s/^HamleDT::Test:://);
     chomp;
     my ($test,$file) = split;
-    $file =~ /hamledt\/([a-z]+)/
+    $file =~ /hamledt\/([-a-z0-9]+)/
         or die "file doesn't match expected pattern: $file";
-    my $language = $1;
-    $count{$language}{$test}++;
-    $count{$language}{TOTAL}++;
+    my $treebank = $1;
+    $count{$treebank}{$test}++;
+    $count{$treebank}{TOTAL}++;
     $count{TOTAL}{$test}++;
     $count{TOTAL}{TOTAL}++;
     $test{$test} = 1;
 }
 
-my @languages = sort keys %count;
+my @treebanks = sort keys %count;
 my @tests = (sort( keys %test), 'TOTAL');
 
 use Text::Table;
 
 my $tb = Text::Table->new(
-        'Test / Language', @languages,
+        'Test / Treebank', @treebanks,
     );
 
 $tb->load(
     map {
         my $test = $_;
-        [$_, map {$count{$_}{$test} || 0} @languages]
+        [$_, map {$count{$_}{$test} || 0} @treebanks]
     } @tests
 );
 
