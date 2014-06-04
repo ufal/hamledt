385b4d568fad48e942157351ad7cf1c011f3c1fc
Jan Mašek <honza.masek@gmail.com>
2014-06-04 20:14:59 +0000

    work in progress

--- statistics/scripts/inconsistencies_POS_surface.pl
+++ statistics/scripts/inconsistencies_POS_surface.pl
@@ -4,36 +4,78 @@ use strict;
 
 use open qw( :std :utf8 );
 
-use List::Util qw( sum );
+use List::MoreUtils qw( any );
 use Getopt::Std;
+use autodie;
 
-our($opt_n);
-getopts('n:');
+our($opt_f);
+getopts('f:');
 
-my $n = $opt_n-1;
+my $source_file = $opt_f;
 
 my %count;
-my %ngrams;
-my %address;
+my %unigrams;
 
-while( defined( my $line =  <> ) ) {
+open my $SURFACE, '<:encoding(utf-8)', $source_file;
+while( defined( my $line = <$SURFACE> ) ) {
     chomp $line;
     my ($file, $forms, $IDs, $iset_feats) = split "\t", $line;
     my @forms = split ' ', $forms;
     my @IDs = split ' ', $IDs;
     my @iset_feats = split ' ', $iset_feats;
-    for my $start (0..$#forms-$n) {
-        my $end = $start + $n;
-        my $ngram = join ' ', @forms[$start..$end];
-        my $iset_feat = join ' ', @iset_feats[$start..$end];
-        $ngrams{$ngram}{$iset_feat}++;
-        $count{$ngram}++;
-        push @{ $address{$ngram}{$iset_feat} },
-            join(' ', $file, @IDs[$start..$end]);
-        }
+    for my $i (0..$#forms) {
+        my $unigram = $forms[$i];
+        my $ID = $IDs[$i];
+        my $iset_feats = $iset_feats[$i];
+        $unigrams{$unigram}{$iset_feats}++;
+        $count{$unigram}++;
+#        my $address = $file . '##' . $ID;
+#        $addresses{$address}
+    }
+}
+close $SURFACE;
+
+# keep only unigrams with at least two different sets of iset features
+for my $unigram (keys %unigrams) {
+    if ( scalar keys %{ $unigrams{$unigram} } < 2 ) {
+        delete $unigrams{$unigram};
+    }
 }
 
-for my $ngram ( sort { $count{$b} <=> $count{$a} }
+my %ngrams;
+open $SURFACE, '<:encoding(utf-8)', $source_file;
+while( defined( my $line = <$SURFACE> ) ) {
+    chomp $line;
+#    print STDERR "$. ";
+    my ($file, $forms, $IDs, $iset_feats) = split "\t", $line;
+    my @forms = split ' ', $forms;
+    my @IDs = split ' ', $IDs;
+    my @iset_feats = split ' ', $iset_feats;
+    for my $start (0..$#forms) {
+        for my $end ($start..$#forms) {
+            next unless ( any { $unigrams{$_} } @forms[$start..$end] );
+            my $ngram = join ' ', @forms[$start..$end];
+            my $iset_feats = join ' ', @iset_feats[$start..$end];
+            $ngrams{$ngram}{$iset_feats}++;
+            $count{$ngram}++;
+        }
+    }
+}
+close $SURFACE;
+
+
+#print STDERR "NGRAMS LOADED\n";
+#__END__
+
+for my $ngram (keys %ngrams) {
+    if ( scalar keys %{ $ngrams{$ngram} } < 2 ) {
+        delete $ngrams{$ngram};
+    }
+}
+
+# print STDERR "NGRAMS PRUNED\n";
+
+for my $ngram ( sort { scalar(@{ [split ' ', $b] }) <=> scalar(@{ [split ' ', $a] }) or $count{$b} <=> $count{$a} }
                     grep { scalar keys %{$ngrams{$_}} > 1 }
                         keys %ngrams ) {
     print "------$ngram------\n";
