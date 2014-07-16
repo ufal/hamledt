d5fddeb1f1df1d04536bc1ee1650c52905b5ab38
Daniel Zeman <zeman@ufal.mff.cuni.cz>
2014-07-16 19:15:51 +0000

    Newer and cleaner version of the Croatian data.

--- normalize/conll_break_cycles.pl
+++ normalize/conll_break_cycles.pl
@@ -3,6 +3,8 @@
 # Copyright © 2011 Dan Zeman <zeman@ufal.mff.cuni.cz>
 # License: GNU GPL
 
+use strict;
+use warnings;
 use utf8;
 use open ':utf8';
 binmode(STDIN, ':utf8');
@@ -10,18 +12,20 @@ binmode(STDOUT, ':utf8');
 binmode(STDERR, ':utf8');
 
 # Empty first element of @tokens corresponds to the artificial root node.
-@tokens = ([]);
+my @tokens = ([]);
 while(<>)
 {
+    # Remove the line break.
+    s/\r?\n$//;
     # Empty line separates sentences.
     if(m/^\s*$/)
     {
-        if(@tokens)
+        if(scalar(@tokens)>1)
         {
             # $ord, $form, $lemma, $cpos, $pos, $feat, $head, $deprel, $phead, $pdeprel
             # Build the tree, watch for cycles.
             my @parents = map {$_->[6]} (@tokens);
-            for(my $i = 0; $i<=$#parents; $i++)
+            for(my $i = 1; $i<=$#parents; $i++)
             {
                 # Start at the i-th node, go to root, watch for cycles.
                 my @map;
@@ -44,19 +48,25 @@ while(<>)
                     }
                 }
             }
-            # Write the corrected tree.
-            foreach my $token (@tokens)
+            # Write the corrected tree. Skip the root node ($tokens[0])!
+            for(my $i = 1; $i<=$#tokens; $i++)
             {
-                print(join("\t", @{$token}), "\n");
+                print(join("\t", @{$tokens[$i]}), "\n");
             }
             print("\n");
         }
+        # Erase all tokens but keep the artificial root node at $tokens[0].
         @tokens = ([]);
     }
     else
     {
-        s/\r?\n$//;
         my @token = split(/\t/, $_);
+        my $n = scalar(@token);
+        if($n!=10)
+        {
+            print STDERR ("WARNING! A CoNLL line (token) should have 10 fields but this one has $n:\n");
+            print STDERR ("$_\n");
+        }
         push(@tokens, \@token);
     }
 }
--- normalize/hr/README.txt
+++ normalize/hr/README.txt
@@ -1,15 +1,17 @@
 SETimes.HR treebank (Croatian)
 http://nlp.ffzg.hr/resources/corpora/setimes-hr/
+We got from Željko Agić on 16 July 2014 a newer and cleaner version than the one that was available on-line at the moment.
 
 License: CC BY-SA 3.0
 
 The original data was not split to training and test. We take the first 90% for training and the rest for testing.
 
-original size = 7995 sentences, 178981 tokens
-training size = 2241 sentences,  50853 tokens
-test size     =  249 sentences,   5571 tokens
+original size = 3736 sentences, 83640 tokens
+training size = 3362 sentences, 75236 tokens
+test size     =  374 sentences,  8404 tokens
 
 The corpus is available in the CoNLL-X (CoNLL 2006) file format.
+Lemmas and morphosyntactic tags have been probably assigned manually, at least in part of the corpus (according to their website).
 Morphosyntactic tags use the Multext-East 4.0 tagset.
 For morphological tagset description: http://nlp.ffzg.hr/data/tagging/msd-hr.html
 
