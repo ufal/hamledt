b46be201da5d9baee4d0dd9f35f025bb2a844114
Daniel Zeman <zeman@ufal.mff.cuni.cz>
2015-03-18 22:05:07 +0000

    Punctuation, symbols and apposition.

---
+++ tests/testexamples.bat
@@ -0,0 +1,7 @@
+rem cmd.exe /c "C:\Users\Dan\Desktop\tred.bat" -l C:\Users\Dan\Documents\Lingvistika\Projekty\tectomt\treex\devel\hamledt\tests\examples\index.fl
+
+set PATH=C:\PROGRA~2\tred\c\bin;C:\PROGRA~2\tred\bin;C:\PROGRA~2\tred\dependencies\bin;%PATH%
+set PERL5LIB=C:\PROGRA~2\tred\dependencies\lib\perl5;C:\PROGRA~2\tred\dependencies\lib\perl5\MSWin32-x86-multi-thread;%PERL5LIB%
+set TRED_DIR=C:\PROGRA~2\tred
+
+C:\strawberry\perl\bin\perl.exe "C:\Program Files (x86)\tred\tred" -l C:\Users\Dan\Documents\Lingvistika\Projekty\tectomt\treex\devel\hamledt\tests\examples\index.fl
