3fa7b6d62d8d07d3d3b062cc3d9f4201c40d08b4
Dan Zeman <zeman@ufal.mff.cuni.cz>
2016-09-19 14:23:24 +0200

    Adding instructions for Windows.

--- tests/filter_test_log.pl
+++ tests/filter_test_log.pl
@@ -4,6 +4,13 @@
 # copy of the tested files).
 # May also filter only hits of one selected test.
 
+###!!! WINDOWS
+# Make all paths relative (erase up to and including the last slash).
+# On Windows, put it to the folder where the target Treex files are.
+# Open cmd.exe and go to the folder with Tred (e.g. cd "C:\Program Files (x86)\tred").
+# Run tred with the -l option pointing to the file list, e.g.
+# tred.bat -l C:\Users\Dan\Documents\Lingvistika\Projects\...\copula.fl
+
 my $only_test_re = '.*'; # try 'UD::UnconvertedDependencies'
 my $only_data_re = '.*'; # try '/net/work/people/zeman/unidep/UD_Uyghur/manually-checked-treex/'
 my $replace_path = ''; # if $only_data_re is the full path except file name, this will make the file name relative to the working folder
