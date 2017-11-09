74bdb1cc7f72f0c028564dc385056551fee1483b
Dan Zeman <zeman@ufal.mff.cuni.cz>
2017-11-09 11:59:37 +0100

    Data split.

--- normalize/cs-fictree/catsource.sh
+++ normalize/cs-fictree/catsource.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-for i in Fictree-shuffled/*.conll ; do
+rm -f data/source/train.conll
+for i in Fictree-shuffled/train-*.conll ; do
   bn=`basename $i .conll`
   cat $i | perl -CDS -e \
-    '$n = 1; while(<>) { if(m/^\s*$/) { $n++ } else { s/^(.*?\t.*?\t.*?\t.*?\t.*?\t)_/$1sid='$bn'-s$n/ } print }'
+    '$n = 1; while(<>) { if(m/^\s*$/) { $n++ } else { s/^(.*?\t.*?\t.*?\t.*?\t.*?\t)_/$1sid='$bn'-s$n/ } print }' \
+    >> data/source/train.conll
+done
+
+rm -f data/source/dev.conll
+for i in Fictree-shuffled/dev-*.conll ; do
+  bn=`basename $i .conll`
+  cat $i | perl -CDS -e \
+    '$n = 1; while(<>) { if(m/^\s*$/) { $n++ } else { s/^(.*?\t.*?\t.*?\t.*?\t.*?\t)_/$1sid='$bn'-s$n/ } print }' \
+    >> data/source/dev.conll
+done
+
+rm -f data/source/test.conll
+for i in Fictree-shuffled/test-*.conll ; do
+  bn=`basename $i .conll`
+  cat $i | perl -CDS -e \
+    '$n = 1; while(<>) { if(m/^\s*$/) { $n++ } else { s/^(.*?\t.*?\t.*?\t.*?\t.*?\t)_/$1sid='$bn'-s$n/ } print }' \
+    >> data/source/test.conll
 done
 
